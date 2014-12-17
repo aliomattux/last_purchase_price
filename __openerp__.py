245e7aba0e7292e9b0d46799b97c817dc315327c
aliomattux <kyle.waid@gcotech.com>
2014-12-16 22:39:43 -0600

    Last purchase price costing method

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Last Purchase Price',
+    'version': '1.1',
+    'author': 'Kyle Waid',
+    'category': 'Sales Management',
+    'depends': ['purchase'],
+    'website': 'https://www.gcotech.com',
+    'description': """ 
+    """,
+    'data': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
