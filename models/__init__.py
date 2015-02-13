8a56f06d5e5b34a242007cf9c0e267aee05a7aec
aliomattux <kyle.waid@gcotech.com>
2015-02-12 22:04:03 -0800

    add refactored version

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ import account_invoice
 import purchase
 import stock
 import procurement
+import account_voucher
--- models/account_invoice.py
+++ models/account_invoice.py
@@ -5,3 +5,13 @@ class AccountInvoice(osv.osv):
     _columns = {
         'purchase_order': fields.many2one('purchase.order', 'Purchase Order'),
     }
+
+
+
+    def invoice_pay_customer(self, cr, uid, ids, context=None):
+	invoice = self.browse(cr, uid, ids[0])
+        vals = super(AccountInvoice, self).invoice_pay_customer(cr, uid, ids, context=context)
+        if not vals['context'].get('default_invoice'):
+	    vals['context']['default_invoice'] = invoice.id
+
+	return vals
